59419c7fad3c5fc433550c0a2fd0b233a1c47e9d
Raphael-Steimvacher <raphaelsteimvacher2251@gmail.com>
2026-07-07 22:24:11 -0300

    feat: fazendo minha primeira gereciamento de rota

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,25 @@
-import http from "http";
+import http from "http"
 
 const server = http.createServer((req, res) => {
-    return res.end('hello world')
+  const { method, url } = req
+
+  if (method === "GET" && url === "/products") {
+    return res.writeHead(200).end("Lista de produtos!")
+  }
+
+  if (method === "POST" && url === "/products") {
+    return res.writeHead(201).end("Produto criado com sucesso!")
+  }
+
+  if (method === "PUT" && url === "/products") {
+    return res.writeHead(200).end("Produto atualizado com sucesso!")
+  }
+
+  if (method === "DELETE" && url === "/products") {
+    return res.writeHead(204).end("Produto deletado com sucesso!")
+  }
+
+  return res.writeHead(404).end("Nenhuma rota encontrada!")
 })
 
 server.listen(3333)
