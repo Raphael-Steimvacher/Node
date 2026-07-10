e7f4fd15061204c779ea76ebc51337bca9b5cfaf
Raphael-Steimvacher <raphaelsteimvacher2251@gmail.com>
2026-07-10 16:10:06 -0300

    feat: montando o tickets object

--- supports-tickets/src/controllers/tickets/create.js
+++ supports-tickets/src/controllers/tickets/create.js
@@ -1,3 +1,16 @@
+import { randomUUID } from "node:crypto"
+
 export function create({ request, response }) {
-  return response.writeHead(201).end(JSON.stringify(request.body))
+  const { equipment, description, user_name } = request.body
+
+  const ticket = {
+    id: randomUUID(),
+    equipment,
+    description,
+    user_name,
+    status: "open",
+    created_at: new Date(),
+    updated_at: new Date(),
+  }
+  return response.writeHead(201).end(JSON.stringify(ticket))
 }
