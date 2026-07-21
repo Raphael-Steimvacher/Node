a7d8873a90c7ba44c1f5f3cdd759e6c77d26ee8a
Raphael-steimvacher <raphaelsteimvacher2251@gmail.com>
2026-07-20 23:16:12 -0300

    Atualizano os dados do banco

--- supports-tickets/src/controllers/tickets/update.js
+++ supports-tickets/src/controllers/tickets/update.js
@@ -1,3 +1,13 @@
 export function update({ request, response, database }) {
-  return response.end("Atualizado com sucesso!")
+  const { id } = request.params
+  const { equipment, description, status } = request.body
+
+  database.update("tickets", id, {
+    equipment,
+    description,
+    status,
+    updated_at: new Date(),
+  })
+
+  return response.writeHead(201).end()
 }
