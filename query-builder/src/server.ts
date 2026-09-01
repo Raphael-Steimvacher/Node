de17d254182bc03b3ca3bf53460e0619ad85ff60
Raphael-steimvacher <raphaelsteimvacher2251@gmail.com>
2026-08-31 23:42:37 -0300

    update and delete in query builder

--- query-builder/src/server.ts
+++ query-builder/src/server.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", async (request: Request, response: Response) => {
-    response.json({ message: "Hello World!" });
+    return response.json({ message: "Hello World!" });
 });
 
 app.post("/courses", async (request: Request, response: Response) => {
@@ -14,14 +14,31 @@ app.post("/courses", async (request: Request, response: Response) => {
     await knex("courses").insert({ name });
     // await knex.raw("INSERT INTO courses (name) VALUES (?)", [name]);
 
-    response.status(201).json(name);
+    return response.status(201).json(name);
 });
 
 app.get("/courses", async (request: Request, response: Response) => {
     const courses = await knex("courses").select().orderBy("name");
     // const courses = await knex.raw("SELECT * FROM courses");
 
-    response.json(courses);
+    return response.json(courses);
+});
+
+app.put("/courses/:id", async (request: Request, response: Response) => {
+    const { id } = request.params;
+    const { name } = request.body;
+
+    await knex("courses").update({ name }).where({ id });
+
+    return response.status(204).send();
+});
+
+app.delete("/courses/:id", async (request: Request, response: Response) => {
+    const { id } = request.params;
+
+    await knex("courses").delete().where({ id });
+
+    return response.status(204).send();
 });
 
 app.listen(3333, () => console.log(`Server is running on port 3333`));
