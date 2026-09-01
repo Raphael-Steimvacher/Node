981ab9ea8f3b9e4e1b994fea68d130a8c2e9d05a
Raphael-steimvacher <raphaelsteimvacher2251@gmail.com>
2026-08-31 23:35:35 -0300

    insert and select in query builder

--- query-builder/src/server.ts
+++ query-builder/src/server.ts
@@ -1,10 +1,27 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from "express";
+import knex from "./database/knex";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 app.get("/", async (request: Request, response: Response) => {
-  response.json({ message: "Hello World!" })
-})
+    response.json({ message: "Hello World!" });
+});
 
-app.listen(3333, () => console.log(`Server is running on port 3333`))
+app.post("/courses", async (request: Request, response: Response) => {
+    const { name } = request.body;
+
+    await knex("courses").insert({ name });
+    // await knex.raw("INSERT INTO courses (name) VALUES (?)", [name]);
+
+    response.status(201).json(name);
+});
+
+app.get("/courses", async (request: Request, response: Response) => {
+    const courses = await knex("courses").select().orderBy("name");
+    // const courses = await knex.raw("SELECT * FROM courses");
+
+    response.json(courses);
+});
+
+app.listen(3333, () => console.log(`Server is running on port 3333`));
